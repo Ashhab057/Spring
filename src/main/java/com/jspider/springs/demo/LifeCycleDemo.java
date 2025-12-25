67186e5eb54a52f1ffd0d373341c0d1e85f5a4dc
Ashhab057 <aec.cse.ashhabjamal@gmail.com>
2025-12-25 14:29:16 +0530

    Spring

---
+++ src/main/java/com/jspider/springs/demo/LifeCycleDemo.java
@@ -0,0 +1,28 @@
+package com.jspider.springs.demo;
+
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class LifeCycleDemo implements
+        InitializingBean,
+        BeanNameAware,
+        DisposableBean{
+
+
+    @Override
+    public void setBeanName(String name) {
+
+    }
+
+    @Override
+    public void destroy() throws Exception {
+
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+    }
+}
+
